96fb48e5b7a9064c65b3e335e9a4c92ed3b66cb0
Josh Nussbaum <joshnuss@gmail.com>
2024-05-30 09:35:01 -0400

    Initial commit

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,32 @@
-import { describe, it, expect } from 'vitest'
+import { describe, test, expect, vi } from 'vitest'
+import { redirections } from './lib'
 
-describe('sum test', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3)
+describe('redirections', () => {
+  const resolve = vi.fn()
+  const handler = redirections({
+    '/discord': 'https://discord.gg/1234'
+  })
+
+  test('when path matches rule, it redirects', () => {
+    const event = { url: new URL('https://localhost:5173/discord') }
+
+    try {
+      handler({ event, resolve })
+    } catch (redirect) {
+      expect(redirect.status).toBe(303)
+      expect(redirect.location).toBe('https://discord.gg/1234')
+      expect(resolve).not.toHaveBeenCalled()
+      return
+    }
+
+    throw Error('did not redirect')
+  })
+
+  test("when path doesn't match rule, it doesn't redirect", () => {
+    const event = { url: new URL('https://localhost:5173/contact') }
+
+    handler({ event, resolve })
+
+    expect(resolve).toHaveBeenCalledWith(event)
   })
 })
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,14 @@
-// Reexport your entry components here
+import type { Handle } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
+
+export function redirections(rules: Record<string, string>): Handle {
+  return ({ event, resolve }) => {
+    const url = rules[event.url.pathname]
+
+    if (url) {
+      redirect(303, url)
+    }
+
+    return resolve(event)
+  }
+}
